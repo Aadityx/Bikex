b6084ad4a503b54236b48232784202fd81b6956e
Aadityx <aadityapatil784@gmail.com>
2026-01-12 16:32:16 +0530

    items added

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,34 @@ function App() {
   const [products, setProducts] = useState([
     {
       id: 1,
-      name: "Wireless Headphones",
+      name: "MT Helmet",
       price: 2999,
       image: "/images/helmet-img.webp"
     },
     {
       id: 2,
-      name: "Smart Watch",
+      name: "Action Camera",
       price: 4999,
-      image: "/images/helmet-img.webp"
+      image: "/images/action-camera.png"
+    },
+    {
+      id: 3,
+      name: "Bike Gaurd",
+      price: 12999,
+      image: "/images/bike-accessories.jpg"
+    },
+    {
+      id: 4,
+      name: "Riding Jacket",
+      price: 14999,
+      image: "/images/jacket-img.webp"
     }
   ]);
 
   return (
     <div className="app">
       <header className="navbar">
-        <h1>Mini E-Commerce</h1>
+        <h1>BikeX</h1>
 
         <div className="nav-buttons">
           <button
